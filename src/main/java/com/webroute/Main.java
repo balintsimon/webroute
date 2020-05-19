3b9f5139235ba9ca217ea4d96b9b25267a4c0e79
balintsimon <balladin0@gmail.com>
2020-05-19 11:47:07 +0200

    add dumb line to try SHH connection

--- src/main/java/com/webroute/Main.java
+++ src/main/java/com/webroute/Main.java
@@ -3,5 +3,6 @@ package com.webroute;
 public class Main {
     public static void main(String[] args) {
         System.out.println("we are here");
+        System.out.println("try this out");
     }
 }
