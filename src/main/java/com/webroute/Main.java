f32830fbbbe5cfc2d397f9e42247533fce59d144
balintsimon <balladin0@gmail.com>
2020-05-19 14:08:21 +0200

    add Oracle docs sample code

--- src/main/java/com/webroute/Main.java
+++
@@ -1,8 +0,0 @@
-package com.webroute;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("we are here");
-        System.out.println("try this out");
-    }
-}
